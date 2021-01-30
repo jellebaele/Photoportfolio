3f3b79f5a576d823661b0e0538fc117c3a8f7dfa
Jelle Baele <jellebaele@hotmail.com>
2021-01-30 15:02:29 +0100

    basic ejs setup from template

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+let app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
