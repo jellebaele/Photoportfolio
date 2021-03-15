3cd4dff419d43d0113a1f2b96bfb17c8b68b9ba6
Jelle Baele <jellebaele@hotmail.com>
2021-03-15 19:02:17 +0100

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 
 app.use(routes);
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1338;
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
