3e87c6c92bd410ef91faed663fb2995d72c22261
Jelle Baele <jellebaele@hotmail.com>
2021-03-20 12:40:06 +0100

    Display selected images

--- public/js/upload-images.js
+++ public/js/upload-images.js
@@ -9,7 +9,7 @@ document.getElementById("uploadButton").addEventListener("click", () => {
 selectedFilesTag.addEventListener("change", () => {
   let numberOfFiles = parseInt(selectedFilesTag.files.length);
 
-    console.log(selectedFileNames.clientWidth);
+  console.log(selectedFileNames.clientWidth);
 
   if (numberOfFiles == 1) {
     selectedFileNames.innerHTML = selectedFilesTag.files[0].name;
@@ -24,22 +24,33 @@ selectedFilesTag.addEventListener("change", () => {
 });
 
 let drawImagesOnScreen = (images) => {
-    for (let i = 0; i < images.length; i++) {
-        let image = document.createElement("img");
-        let reader = new FileReader();
+  let container = document.getElementsByClassName("container")[0];
+  for (let i = 0; i < images.length; i++) {
+    let currImage = images[i];
+    
+    let imageContainer = document.createElement("div");
+    imageContainer.setAttribute("class", "image-container");
 
-        reader.onload = (event) => {
-            image.src = event.target.result;
-            document.body.appendChild(image);
-        }
-              
-        reader.readAsDataURL(images[i]);
-    }
-}
+    let image = document.createElement("img");
+    let title = document.createElement("p");
+    title.setAttribute("class", "img-title");
+    title.innerHTML = currImage.name;
 
-let deleteImagesOnScreen = () => {
-  
-}
+    let reader = new FileReader();
+    reader.onload = (event) => {
+      image.src = event.target.result;
+      imageContainer.appendChild(image);
+      imageContainer.appendChild(title);
+      container.appendChild(imageContainer);
+    };
+
+    reader.readAsDataURL(currImage);
+  }
+};
+
+let createImageElement = () => {};
+
+let deleteImagesOnScreen = () => {};
 
 document.getElementById("submit").addEventListener("click", () => {
   if (selectedFilesTag.value === "") {
