31c7388106c0b62aafbc4ee031a5d36f2e7702e4
Jelle Baele <jellebaele@hotmail.com>
2021-03-26 16:43:10 +0100

    Implemented backend basic upload with multer (todo: implement mongodb)

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,13 +1,36 @@
+const multer = require("multer");
+const path = require("path");
+const util = require("util");
+
+const storageThumbnail = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, path.join(`${__dirname}/../uploads/thumbnail`));
+  },
+  filename: (req, file, callback) => {
+    const match = ["image/png", "image/jpeg"];
+    if (match.indexOf(file.mimetype) === -1) {
+      var message = `<strong>${file.originalname}</strong> is invalid. Only .png/.jpeg files are accepted`;
+      return callback(message, null);
+    }
+
+    let filename = `${file.originalname}`;
+    callback(null, filename);
+  },
+});
+
+let uploadFiles = multer({ storage: storageThumbnail }).array("myfile", 10);
+
 const getIndex = (req, res) => {
-    res.render("pages/upload-images");
-}
+  res.render("pages/upload-images");
+};
 
 const postImages = (req, res) => {
-    // Use multer to upload -> req.file will work with right name!
-    console.log(req.file);
-}
+    res.status(200);
+    res.send();
+};
 
 module.exports = {
-    getIndex,
-    postImages
-}
+  getIndex,
+  postImages,
+  uploadFiles,
+};
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const uploadController = require('../controllers/uploadController')
 
 router.get("/", uploadController.getIndex);
-router.post("/", uploadController.postImages);
+router.post("/", uploadController.uploadFiles, uploadController.postImages);
 
 module.exports = router;
