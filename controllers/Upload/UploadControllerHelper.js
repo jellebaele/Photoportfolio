60b5226bd0a11de6d8e658e624836011bd8c8098
Jelle Baele <jellebaele@hotmail.com>
2021-04-21 22:30:38 +0200

    Create or update category upon image upload and move to helper class

---
+++ controllers/Upload/UploadControllerHelper.js
@@ -0,0 +1,72 @@
+const ImageModel = require("../../models/Image");
+const CategoryModel = require("../../models/Category");
+
+class uploadControllerHelper {
+  async SaveNewImage(image, category, newImages) {
+    const newImage = new ImageModel({
+      title: image.originalname,
+      img: {
+        path: image.path,
+        mimetype: image.mimetype,
+        size: image.size,
+        encoding: image.encoding,
+      },
+      category: await this.UpdateOrCreateCategory(category),
+      index: await this.GetNewIndex(),
+    });
+    newImages.push(newImage);
+    await newImage.save();
+  }
+
+  async UpdateOrCreateCategory(category) {
+    return new Promise((resolve, reject) => {
+      CategoryModel.find({ title: category })
+        .limit(1)
+        .then((result) => {
+          if (result.length < 1) {
+            const newCategory = this.CreateNewCategory(category);
+            resolve(newCategory.title);
+          } else {
+            this.UpdateCategory(category, result[0].amountOfPictures)
+              .then(() => resolve(result[0].title))
+              .catch((error) => reject(error));
+          }
+        })
+        .catch((error) => reject(error));
+    });
+  }
+
+  async CreateNewCategory(category) {
+    const newCategory = new CategoryModel({
+      title: category,
+      amountOfPictures: 1,
+    });
+    await newCategory.save();
+    return newCategory;
+  }
+
+  async UpdateCategory(category, amountOfPictures) {
+    const filter = { title: category };
+    const updateCategory = {
+      $set: {
+        amountOfPictures: ++amountOfPictures,
+      },
+    };
+    return await CategoryModel.updateOne(filter, updateCategory);
+  }
+
+  async GetNewIndex() {
+    return new Promise((resolve, reject) => {
+      ImageModel.find()
+        .sort({ $natural: -1 })
+        .limit(1)
+        .then((result) => {
+          if (result.length > 0) resolve(result[0].index + 1);
+          else resolve(0);
+        })
+        .catch((error) => reject(error));
+    });
+  }
+}
+
+module.exports = uploadControllerHelper;
--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,104 +1,62 @@
 const multer = require("multer");
 const path = require("path");
-const Image = require("../models/Image");
-const util = require("util");
-const { rejects } = require("assert");
+const ImageModel = require("../models/Image");
+const CategoryModel = require("../models/Category");
+const { resolve } = require("path");
+const UploadControllerHelper = require("./Upload/UploadControllerHelper");
 
 const storageThumbnail = multer.diskStorage({
-  destination: (req, file, callback) => {
-    // If req.body.category ... set destination ...
-    // Enum maken en vergelijken met req.body.category
-    callback(null, path.join(`${__dirname}/../uploads/thumbnail`));
-  },
-  filename: (req, file, callback) => {
-    const match = ["image/png", "image/jpeg"];
-    if (match.indexOf(file.mimetype) === -1) {
-      var message = `<strong>${file.originalname}</strong> is invalid. Only .png/.jpeg files are accepted`;
-      return callback(message, null);
-    }
+   destination: (req, file, callback) => {
+      // If req.body.category ... set destination ...
+      // Enum maken en vergelijken met req.body.category
+      callback(null, path.join(`${__dirname}/../uploads/thumbnail`));
+   },
+   filename: (req, file, callback) => {
+      const match = ["image/png", "image/jpeg"];
+      if (match.indexOf(file.mimetype) === -1) {
+         var message = `<strong>${file.originalname}</strong> is invalid. Only .png/.jpeg files are accepted`;
+         return callback(message, null);
+      }
 
-    let filename = file.originalname.replace(/[*$µ£`´#^¨|]/g, "");
-    callback(null, filename);
-  },
+      let filename = file.originalname.replace(/[*$µ£`´#^¨|]/g, "");
+      callback(null, filename);
+   },
 });
 
 let uploadFiles = multer({ storage: storageThumbnail }).array("files", 10);
 
 const getIndex = (req, res) => {
-  res.render("pages/upload-images");
+   res.render("pages/upload-images");
 };
 
 const postImages = (req, res) => {
-  try {
-    const newImages = CreateNewImages(req);
-    res.json({ images: newImages });
-    res.status(200);
-    res.send();
-  } catch (error) {
-    res.status(501);
-    res.send();
-  }
+   try {
+      const newImages = SaveNewImages(req);
+      res.json({ images: newImages });
+      res.status(200).send();
+   } catch (error) {
+      res.status(501).send();
+   }
 };
 
-async function CreateNewImages(req) {
-  try {
-    let newImages = [];
-    for (const image of req.files) {
-      const newImage = new Image({
-        title: image.originalname,
-        img: {
-          path: image.path,
-          mimetype: image.mimetype,
-          size: image.size,
-          encoding: image.encoding,
-        },
-        category: req.body.category,
-        index: await getNewIndex()
-      });
-      newImages.push(newImage);
-      await newImage.save();
-    }
-    return newImages;
-  } catch (error) {
-    console.log("Something went wrong " + error);
-    return error;
-  }
-}
+async function SaveNewImages(req) {
+   try {
+      let newImages = [];
+      let uploadHelper = new UploadControllerHelper();
 
-async function getNewIndex() {
-  return new Promise((resolve, reject) => {
-    Image.find()
-      .sort({ $natural: -1 })
-      .limit(1)
-      .then((result) => {
-        if (result.length > 0) resolve(result[0].index + 1);
-        else resolve(0);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-}
+      for (const image of req.files) {
+         await uploadHelper.SaveNewImage(image, req.body.category, newImages);
+      }
 
-function getNewImageIndex() {
-  return new Promise((resolve, reject) => {
-    lastAddedImage = Image.find()
-      .limit(1)
-      .sort({ $natural: -1 })
-      .then(
-        (result) => {
-          if (result.length === 1) resolve(99);
-          else resolve(0);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-  });
+      return newImages;
+   } catch (error) {
+      console.log("Something went wrong " + error);
+      return error;
+   }
 }
 
 module.exports = {
-  getIndex,
-  postImages,
-  uploadFiles,
+   getIndex,
+   postImages,
+   uploadFiles,
 };
