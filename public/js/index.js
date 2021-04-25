ace1a92853d3c97d4f7c4bee4a77395efd3caf74
Jelle Baele <jellebaele@hotmail.com>
2021-04-25 13:20:44 +0200

    Hide animation when necessary

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-//alert("test");
+window.addEventListener('load', () => {
+  const preload = document.querySelector('.preload');
+  preload.classList.add('preload-finish');
+  document.getElementsByTagName("body")[0].style = 'overflow: auto';
+})
