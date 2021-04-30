5a3b5223a80ee2ad7b6d7880c0b46c7e44de0fcd
Jelle Baele <jellebaele@hotmail.com>
2021-04-30 20:39:54 +0200

    Deleted unnecesseray comments

--- routes/subroutes/overviewRoute.js
+++ routes/subroutes/overviewRoute.js
@@ -2,7 +2,5 @@ const router = require("express").Router();
 const overviewController = require('../../controllers/overviewController')
 
 router.get("/", overviewController.getIndex)
-//router.get("/", uploadController.getIndex);
-//router.post("/", uploadController.uploadFiles, uploadController.postImages)
 
 module.exports = router;
