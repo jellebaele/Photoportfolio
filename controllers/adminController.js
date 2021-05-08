9dc1aa2e16c659d73b677bbf77d572e552a11bc1
Jelle Baele <jellebaele@hotmail.com>
2021-05-08 14:05:01 +0200

    Created new admin route for categories

---
+++ controllers/adminController.js
@@ -0,0 +1,7 @@
+function getIndex(req, res) {
+    res.render("pages/admin/categories");
+ }
+
+ module.exports = {
+    getIndex,
+ };
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const apiRoute = require("./subroutes/apiRoute");
 const overviewRoute = require("./subroutes/overviewRoute");
+const adminRoute = require("./subroutes/adminRoute");
 
 router.get("/about", function (req, res) {
   res.render("pages/about");
 });
 
 router.use("/home", overviewRoute);
+router.use("/admin", adminRoute);
 router.use("/api", apiRoute);
 
 module.exports = router;
