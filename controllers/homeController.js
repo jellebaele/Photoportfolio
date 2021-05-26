3348e41df5d8e41c6832396fd52259017028ef72
Jelle Baele <jellebaele@hotmail.com>
2021-05-26 22:49:50 +0200

    Used repository instead of local function

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,12 @@
+const CategoryRepository = require("../repository/CategoryRepository");
 const ImageModel = require("../models/Image");
-const CategoryModel = require("../models/Category");
+const categoryRepository = new CategoryRepository();
 
 function getIndex(req, res) {
-   getCategory("Thumbnail")
-      .then((category) => {
-         ImageModel.find({ category: category })
+   categoryRepository.searchCategory("Thumbnail")
+      .then(category => {
+         if (category.length > 0) {
+            ImageModel.find({ category: category[0].title })
             .then((images) => {
                res.render("pages/index", { images: images });
             })
@@ -12,24 +14,13 @@ function getIndex(req, res) {
                console.log(error);
                res.status(500).send("An error occurred", error);
             });
+         }         
       })
       .catch((error) => {
          console.log(error);
          res.status(500);
          res.render("pages/index", { images: [] });
       });
-}
-
-async function getCategory(categoryToBeFound) {
-   try {
-      const category = await CategoryModel.find({
-         title: categoryToBeFound,
-      }).limit(1);
-      if (category.length > 0) return category[0].title;
-      else throw new Error(`Category '${categoryToBeFound}' not found!`);
-   } catch (error) {
-      throw error;
-   }
 }
 
 module.exports = {
