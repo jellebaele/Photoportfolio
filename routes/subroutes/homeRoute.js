91ec3e8686fb036755e4c5af3a26e21f361c1c70
Jelle Baele <jellebaele@hotmail.com>
2021-05-26 21:49:39 +0200

    Deleted unnecessary 'get'

--- routes/subroutes/homeRoute.js
+++ routes/subroutes/homeRoute.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const overviewController = require('../../controllers/homeController')
 
 router.get("/", overviewController.getIndex);
-router.get("/")
 
 module.exports = router;
