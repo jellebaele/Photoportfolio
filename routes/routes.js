d8a5b89e04ddeaa983fc3e2cb1665b646ae49260
Jelle Baele <jellebaele@hotmail.com>
2021-05-26 21:48:01 +0200

    Renamed 'overviewRoute' to 'homeRoute'

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 const apiRoute = require("./subroutes/apiRoute");
-const overviewRoute = require("./subroutes/overviewRoute");
+const homeRoute = require("./subroutes/homeRoute");
 const adminRoute = require("./subroutes/adminRoute");
+// const categoryRoute = require("../subroutes/categoryRoute");
 
 router.get("/about", function (req, res) {
   res.render("pages/about");
 });
 
-router.use("/home", overviewRoute);
+router.use("/home", homeRoute);
+// router.use("/overview", overviewRoute);
 router.use("/admin", adminRoute);
 router.use("/api", apiRoute);
 
--- routes/subroutes/homeRoute.js
+++ routes/subroutes/homeRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const overviewController = require('../../controllers/overviewController')
+const overviewController = require('../../controllers/homeController')
 
 router.get("/", overviewController.getIndex);
+router.get("/")
 
 module.exports = router;
