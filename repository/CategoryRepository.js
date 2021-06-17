347d59b6b6b689bab3c848367634ca27a23b6852
Jelle Baele <jellebaele@hotmail.com>
2021-06-17 23:49:30 +0200

    Refactor: Replace promise by async

--- repository/CategoryRepository.js
+++ repository/CategoryRepository.js
@@ -5,155 +5,131 @@ const CategoryModel = require("../models/Category");
 class CategoryRepository {
     async createCategory(categoryTitle, amountOfPictures = 0) {
         if (categoryTitle === "") categoryTitle = "undefined";
+        try {
+            const category = await this.searchCategoryByTitle(categoryTitle);
+            if (category < 1) {
+                const newCategory = await this.createNewCategory(categoryTitle, amountOfPictures);
 
-        return new Promise((resolve, reject) => {
-            this.searchCategoryByTitle(categoryTitle)
-                .then(result => {
-                    if (result < 1) {
-                        this.createNewCategory(categoryTitle, amountOfPictures)
-                            .then(newCategory => resolve({
-                                created: true,
-                                newCategory: newCategory,
-                                existingCategory: undefined,
-                            }))
-                            .catch(error => reject(error));
-                    } else {
-                        resolve({
-                            created: false,
-                            newCategory: undefined,
-                            existingCategory: result[0]
-                        });
-                    }
-                })
-                .catch(error => reject(error))
-        });
+                return {
+                    created: true,
+                    newCategory: newCategory,
+                    existingCategory: undefined
+                }
+            } else {
+                return {
+                    created: false,
+                    newCategory: undefined,
+                    existingCategory: category[0]
+                }
+            }
+        } catch (error) {
+            throw error;
+        }
     }
 
     async searchCategoryByTitle(title) {
-        return new Promise((resolve, reject) => {
-            CategoryModel.find({ title: title })
-                .limit(1)
-                .then((result) => resolve(result))
-                .catch((error) => reject(error))
-        });
+        try {
+            return await CategoryModel.find({ title: title }).limit(1);
+        } catch (error) {
+            throw error;
+        }
     }
 
     async searchCategoryById(id) {
-        return new Promise((resolve, reject) => {
-            CategoryModel.find({ _id: id })
-                .limit(1)
-                .then((result) => {
-                    resolve(result)
-                })
-                .catch((error) => reject(error))
-        });
+        try {
+            return await CategoryModel.find({ _id: id }).limit(1);
+        } catch (error) {
+            throw error;
+        }
     }
 
     async searchCategories(title, limit = 50) {
         if (limit > 50) limit = 50;
 
-        return new Promise((resolve, reject) => {
-            CategoryModel.find({ title: title })
-                .limit(limit)
-                .then((categories) => {
-                    resolve(categories)
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        })
+        try {
+            return await CategoryModel.find({ title: title }).limit(limit);
+        } catch (error) {
+            throw error;
+        }
     }
 
     async createNewCategory(categoryTitle, amountOfPictures) {
-        return new Promise((resolve, reject) => {
+        try {
             const newCategory = new CategoryModel({
                 title: categoryTitle,
                 amountOfPictures: amountOfPictures,
             });
-            newCategory.save()
-                .then((newCategory) => {
-                    this.createDirectory(categoryTitle)
-                        .then(() => resolve(newCategory))
-                        .catch(error => reject(error));
-                })
-                .catch(error => reject(error));
-        });
+
+            const newCategorySaved = await newCategory.save();
+            await this.createDirectory(categoryTitle);
+            return newCategorySaved;
+        } catch (error) {
+            throw error;
+        }
     }
 
     createDirectory(categoryTitle) {
-        return new Promise((resolve, reject) => {
-            fs.mkdir(path.join(`${__dirname}/../uploads/categories/${categoryTitle}`), (err) => {
-                if (err) reject(err);
-                resolve('Directory created')
-            })
+        fs.mkdir(path.join(`${__dirname}/../uploads/categories/${categoryTitle}`), (err) => {
+            if (err) throw err;
         })
     }
 
     removeDirectory(categoryTitle) {
-        return new Promise((resolve, reject) => {
-            fs.rmdir(path.join(`${__dirname}/../uploads/categories/${categoryTitle}`), { recursive: true }, (err) => {
-                if (err) reject(err);
-                else resolve('Directory deleted');
-            })
+        fs.rmdir(path.join(`${__dirname}/../uploads/categories/${categoryTitle}`), { recursive: true }, (err) => {
+            if (err) throw (err);
         })
-
     }
 
     async updateCategoryAmountOfPicturesByTitle(categoryTitle, amountOfPictures) {
-        return new Promise((resolve, reject) => {
-            this.searchCategoryByTitle(categoryTitle)
-                .then(result => {
-                    if (result.length > 0) {
-                        const filter = { title: result[0].title };
-                        const updateCategory = {
-                            $set: {
-                                amountOfPictures: ++result[0].amountOfPictures,
-                            },
-                        };
+        try {
+            const category = await this.searchCategoryByTitle(categoryTitle);
 
-                        CategoryModel.updateOne(filter, updateCategory)
-                            .then(updatedCategory => {
-                                resolve({
-                                    status: updatedCategory,
-                                    updatedCategory: result[0]
-                                })
-                            })
-                            .catch(error => reject(error));
-                    }
-                })
-                .catch(error => reject(error))
-        })
+            if (category.length > 0) {
+                const filter = { title: category[0].title };
+                const updateCategory = {
+                    $set: {
+                        amountOfPictures: ++category[0].amountOfPictures,
+                    },
+                };
+
+                const updatedCategory = await CategoryModel.updateOne(filter, updateCategory);
+                return {
+                    status: updatedCategory,
+                    updatedCategory: category[0]
+                }
+            }
+        } catch (error) {
+            throw error;
+        }
     }
 
     async updateCategoryById(id, newTitle) {
-        return new Promise((resolve, reject) => {
-            const filter = { _id: id };
-            const updateCategory = {
-                $set: { title: newTitle }
-            };
+        const filter = { _id: id };
+        const updateCategory = {
+            $set: { title: newTitle }
+        };
 
-            CategoryModel.updateOne(filter, updateCategory)
-                .then(updatedCategory => {
-                    resolve(updatedCategory)
-                })
-                .catch(error => reject(error))
-        })
+        try {
+            return await CategoryModel.updateOne(filter, updateCategory);
+        } catch (error) {
+            throw error;
+        }
     }
 
     async deleteCategory(id) {
-        let category = await this.searchCategoryById(id);
-        return new Promise((resolve, reject) => {
+        try {
+            const category = await this.searchCategoryById(id);
+
             if (category.length > 0) {
-                CategoryModel.deleteOne({ _id: id })
-                    .then(deletedCategory => {
-                        this.removeDirectory(category[0].title);
-                        resolve(deletedCategory)
-                    })
-                    .catch(error => reject(error));
+                const deletedCategory = await CategoryModel.deleteOne({ _id: id });
+                await this.removeDirectory(category[0].title);;
+                return deletedCategory;
             } else {
-                reject(new Error("Category does not exist"));
+                throw new Error("Category does not exist");
             }
-        })
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
