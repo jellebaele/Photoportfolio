4e65dfd2b8b8b384c7f8a565cdffa835cd69b972
Jelle Baele <jellebaele@hotmail.com>
2021-07-08 09:27:09 +0200

    Change path name of each image when rename

--- repository/ImageRepository.js
+++ repository/ImageRepository.js
@@ -1,17 +1,15 @@
-const fs = require('fs');
 const ImageModel = require("../models/Image");
 const CategoryRepository = require("./CategoryRepository");
 const categoryRepository = new CategoryRepository();
 
 class UploadControllerHelper {
-   async SaveNewImage(image, categoryTitle, newImages, description) {
+   async SaveNewImage(image, resizedImage, categoryTitle, description, newImages) {
       try {
-         // console.log("uploads" + image.path.split("uploads")[1]);
-         // console.log(image);
          const newImage = new ImageModel({
             title: image.originalname,
             img: {
                path_original: "uploads" + image.path.split("uploads")[1],
+               path_resized: "uploads" + resizedImage.path.split("uploads")[1],
                mimetype: image.mimetype,
                size: image.size,
                encoding: image.encoding,
@@ -39,7 +37,7 @@ class UploadControllerHelper {
 
    async GetNewIndex(categoryTitle) {
       try {
-         const lastImageUploaded = await ImageModel.find({ category: categoryTitle}).sort({ $natural: -1 }).limit(1);
+         const lastImageUploaded = await ImageModel.find({ category: categoryTitle }).sort({ $natural: -1 }).limit(1);
          if (lastImageUploaded.length > 0) return lastImageUploaded[0].index + 1;
          else return 0;
       } catch (error) {
@@ -56,13 +54,21 @@ class UploadControllerHelper {
       }
    }
 
-   async updateImagesByCategory(categoryName, newCategoryName) {
-      const filter = { category: categoryName };
-      const updateImage = {
-         $set: { category: newCategoryName }
-      };
-
+   async updateImagesByCategory(oldCategoryName, newCategoryName) {
       try {
+         const imagesToBeUpdated = await this.findImagesByCategory(oldCategoryName, -1);
+         imagesToBeUpdated.forEach(async (image, i) => {
+            image.img.path_original = image.img.path_original.replace(`\\categories\\${oldCategoryName}`, `\\categories\\${newCategoryName}`);
+            image.img.path_resized = image.img.path_resized.replace(`\\categories\\${oldCategoryName}`, `\\categories\\${newCategoryName}`);
+            await image.save();
+         });
+
+         const filter = { category: oldCategoryName };
+         const updateImage = {
+            $set: {
+               category: newCategoryName,
+            }
+         };
          return await ImageModel.updateMany(filter, updateImage);
       } catch (error) {
          throw error;
