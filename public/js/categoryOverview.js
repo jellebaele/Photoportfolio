aad84a286efd9f3fded2b3aa602b3e4dc79dceb4
Jelle Baele <jellebaele@hotmail.com>
2021-08-13 23:44:39 +0200

    Created new class 'ModalCreator'

--- public/js/categoryOverview.js
+++ public/js/categoryOverview.js
@@ -1,19 +1,13 @@
+import ModalCreator from "./modal/ModalCreator.js";
+
 const modal = document.querySelector('.modal');
 const previews = document.querySelectorAll(".gallery img");
 const fullImage = document.querySelector(".full-image");
 
-previews.forEach(preview => {
-  preview.addEventListener('click', () => {
-    modal.classList.add("open");
-    fullImage.classList.add("open");
-    const originalSource = preview.getAttribute("data-original");
-    fullImage.src = originalSource;
-  })
-});
 
-modal.addEventListener('click', (e) => {
-  if (e.target.classList.contains('modal')) {
-    modal.classList.remove('open');
-    fullImage.classList.remove('open');
-  }
-});
+function init() {
+  const modalCreator = new ModalCreator(modal, previews, fullImage);
+  modalCreator.addEventListeners();
+}
+
+init();
---
+++ public/js/modal/ModalCreator.js
@@ -0,0 +1,37 @@
+class ModalCreator {
+  constructor(modal, previews, fullImage) {
+    this.modal = modal;
+    this.previews = previews;
+    this.fullImage = fullImage;
+  }
+
+  addEventListeners() {
+    this.previews.forEach(preview => {
+      preview.addEventListener('click', () => {
+        this.modal.classList.add("open");
+        this.fullImage.classList.add("open");
+
+        const preload = document.querySelector('.preload');
+        preload.classList.remove('preload-finish');
+
+        const originalSource = preview.getAttribute("data-original");
+        this.fullImage.src = originalSource;
+
+        this.fullImage.onload = () => {
+          preload.classList.add('preload-finish');
+        }
+      })
+    });
+
+
+    this.modal.addEventListener('click', (e) => {
+      if (e.target.classList.contains('modal')) {
+        this.modal.classList.remove('open');
+        this.fullImage.classList.remove('open');
+        this.fullImage.src = ""
+      }
+    });
+  }
+}
+
+export default ModalCreator;
