e25957aba4e9fcdc792625557eaac3957bdc9140
Jelle Baele <jellebaele@hotmail.com>
2021-09-29 21:15:21 +0200

    Created dropdown for category

--- public/js/Dom/modal/BodyCreator.js
+++ public/js/Dom/modal/BodyCreator.js
@@ -8,14 +8,14 @@ class BodyCreator {
     }
 
     create(image, id) {
-        this.title = this.createRow("Titel", "title", image.title);
-        this.category = this.createRow("Category", "category", image.category);
-        this.description = this.createRow("Beschrijving", "description", image.description);
-        const originalInfoRow = this.createRowDetailed("Origineel",
+        this.title = this.createTextAreaRow("Titel", "title", image.title);
+        this.category = this.createDropdownRow("Category", "category", image.category);
+        this.description = this.createTextAreaRow("Beschrijving", "description", image.description);
+        const originalInfoRow = this.createDetailedTextAreaDetailed("Origineel",
             this.getSubrowSizeInfo("size-original", "url-original", `${image.img.size_original}kB`, image.img.path_original))
-        const resizedInfoRow = this.createRowDetailed("Origineel",
+        const resizedInfoRow = this.createDetailedTextAreaDetailed("Origineel",
             this.getSubrowSizeInfo("size-resized", "url-resize", `${image.img.size_resized}kB`, image.img.path_resized));
-        const typeRow = this.createRow("Type", "mime-type", image.img.mimetype);
+        const typeRow = this.createTextAreaRow("Type", "mime-type", image.img.mimetype);
         const buttonRow = this.createButtonRow(id);
 
         this.bodyTag.appendChild(this.title);
@@ -36,14 +36,9 @@ class BodyCreator {
         return [{ name: "Grootte", id: idSize, content: size }, { name: "Url", id: idUrl, content: url }];
     }
 
-    createRow(rowName, rowId, rowContent, textFieldClassName = "text-field", isDisabled = true) {
-        const rowTag = document.createElement("div");
-        rowTag.classList.add("modal-row");
-
-        const label = document.createElement("label");
-        label.classList.add(textFieldClassName);
-        label.setAttribute("for", rowId);
-        label.innerHTML = rowName;
+    createTextAreaRow(rowName, rowId, rowContent, textFieldClassName = "text-field", isDisabled = true) {
+        const rowTag = this.createRowTag();
+        const label = this.createLabelTag(textFieldClassName, rowId, rowName);
 
         const textArea = document.createElement("textarea");
         textArea.id = rowId;
@@ -51,6 +46,38 @@ class BodyCreator {
 
         rowTag.appendChild(label);
         rowTag.appendChild(textArea);
+        return rowTag;
+    }
+
+    createDropdownRow(rowName, rowId, rowContent, textFieldClassName = "text-field", isDisabled = true) {
+        const rowTag = this.createRowTag();
+        const label = this.createLabelTag(textFieldClassName, rowId, rowName);
+
+        const dropdown = document.createElement("select");
+        dropdown.name = rowId;
+        dropdown.disabled = isDisabled;
+        
+        const optionElement = document.createElement("option");
+        optionElement.textContent = rowContent;
+        optionElement.value = rowContent;
+        
+        dropdown.appendChild(optionElement);
+        rowTag.appendChild(label);
+        rowTag.appendChild(dropdown);
+        return rowTag;
+    }
+
+    createLabelTag(textFieldClassName, rowId, rowName) {
+        const label = document.createElement("label");
+        label.classList.add(textFieldClassName);
+        label.setAttribute("for", rowId);
+        label.innerHTML = rowName;
+        return label;
+    }
+
+    createRowTag() {
+        const rowTag = document.createElement("div");
+        rowTag.classList.add("modal-row");
         return rowTag;
     }
 
@@ -80,7 +107,7 @@ class BodyCreator {
         textArea.rows = linecount;
     }
 
-    createRowDetailed(mainRowName, subrows, isDisabled = true) {
+    createDetailedTextAreaDetailed(mainRowName, subrows, isDisabled = true) {
         const mainRowTag = document.createElement("div");
         mainRowTag.classList.add("modal-row-detailed");
 
@@ -90,7 +117,7 @@ class BodyCreator {
         mainRowTag.appendChild(mainRowTitleTag);
 
         subrows.forEach(subrow => {
-            const row = this.createRow(subrow.name, subrow.id, subrow.content, "text-field-detailed", isDisabled);
+            const row = this.createTextAreaRow(subrow.name, subrow.id, subrow.content, "text-field-detailed", isDisabled);
             mainRowTag.appendChild(row);
         });
 
